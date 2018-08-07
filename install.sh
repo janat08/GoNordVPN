70b3f595147b2a3f58bd9af94cfe41f0bebcc41f
themester <dgrripoll@gmail.com>
2018-08-08 00:54:25 +0200

    update install script

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ go build -o GoNordVPN
 currentPath=`pwd`
 dirPath=`basename $currentPath`
 cd
-cp -r $currentPath /usr/local/share/$dirPath
+cp -r $currentPath /usr/local/share/
 cat <<EOF > /usr/local/bin/gonordvpn
 #!/bin/bash
 cd "/usr/local/share/$dirPath/" && ./GoNordVPN "\$@"
